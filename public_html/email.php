e18f7f4814d1db33b13f68441e6ac9061620642d
DenzelHadebe <111304704+CutomMade@users.noreply.github.com>
2023-10-17 01:37:29 +0200

    Delete public_html directory

--- public_html/email.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','id20952656_denzel07','@Bozzin2023','id20952656_shop_db') or die('connection failed');
-
-define('SENDGRID_API_KEY','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');
-?>
